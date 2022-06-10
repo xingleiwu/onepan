aae7414979df4e3a9adf357f97b815ce29a2d556
xingleiwu <suihonghui4@gmail.com>
2022-06-10 22:48:27 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -47,7 +47,7 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-//   links: [
+  links: [
 //     {
 //       name: 'GitHub',
 //       link: 'https://github.com/zxfccmm4/onedrive-vercel-index',
@@ -60,7 +60,7 @@ module.exports = {
 //       name: 'twitter',
 //       link: 'https://twitter.com/sevenccmm',
 //     },
-//   ],
+  ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
